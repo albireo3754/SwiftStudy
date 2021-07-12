4e638acc1c0086566c6dd50e2033246f0351d3ab
starpro123 <starpro123@naver.com>
2021-07-12 20:24:41 +0900

    feature: 수정시 저장에서, 앱 종료시 저장으로 로직변경

--- Checklists/Checklists/AllListsViewController.swift
+++ Checklists/Checklists/AllListsViewController.swift
@@ -35,22 +35,12 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     
     let cellIdentifier = "ChecklistCell"
     var lists = [Checklist]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var list = Checklist(name: "Study")
-        lists.append(list)
-
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-
-        list = Checklist(name: "To Do")
-        lists.append(list)
-
+        print(dataFilePath())
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        
+        loadChecklists()
         navigationController?.navigationBar.prefersLargeTitles = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -106,6 +96,49 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
       let indexPaths = [indexPath]
       tableView.deleteRows(at: indexPaths, with: .automatic)
     }
+    
+    override func tableView( _ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath
+    ) {
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(lists)
+            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding item array: \(error.localizedDescription)")
+        }
+    }
+        
+    func loadChecklists() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode([Checklist].self, from: data)
+            } catch {
+                print("Error decoding item array: \(error.localizedDescription)")
+            }
+        }
+    }
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate{
     var checklist: Checklist!
-    
+
+
     func itemDetailViewControllerDidCancel(_ controller: AddItemViewController) {
         
         navigationController?.popViewController(animated: true)
@@ -18,35 +19,33 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     
     func itemDetailViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem) {
         
-        let newRowIndex = items.count
-        items.append(item)
+        let newRowIndex = checklist.items.count
+        checklist.items.append(item)
         
         let indexPath = IndexPath(row: newRowIndex, section: 0)
         let indexPaths = [indexPath]
         tableView.insertRows(at: indexPaths, with: .automatic)
         navigationController?.popViewController(animated: true)
-        saveChecklistItems()
+        
     }
     func itemDetailViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem) {
-        if let index = items.firstIndex(of: item) {
+        if let index = checklist.items.firstIndex(of: item) {
             let indexPath = IndexPath(row: index, section: 0)
             if let cell = tableView.cellForRow(at: indexPath) {
                 configureText(for: cell, with: item)
             }
             navigationController?.popViewController(animated: true)
         }
-        saveChecklistItems()
+        
     }
     
     
     // MARK: - Table View Data Source
-    var items = [ChecklistItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
         title = checklist.name
-        loadChecklistItems()
     }
     
     // MARK: - Navigation
@@ -58,17 +57,17 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
             let controller = segue.destination as! AddItemViewController
             controller.delegate = self
             if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
-                controller.itemToEdit = items[indexPath.row]
+                controller.itemToEdit = checklist.items[indexPath.row]
             }
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return checklist.items.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        let item = items[indexPath.row]
+        let item = checklist.items[indexPath.row]
         configureText(for: cell, with: item)
         configureCheckmark(for: cell, with: item)
         return cell
@@ -76,20 +75,20 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            let item = items[indexPath.row]
+            let item = checklist.items[indexPath.row]
             item.checked.toggle()
             configureCheckmark(for: cell, with: item)
         }
         tableView.deselectRow(at: indexPath, animated: true)
-        saveChecklistItems()
+        
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        items.remove(at: indexPath.row)
+        checklist.items.remove(at: indexPath.row)
         
         let indexPaths = [indexPath]
         tableView.deleteRows(at: indexPaths, with: .automatic)
-        saveChecklistItems()
+        
     }
     
     func configureText(for cell: UITableViewCell, with item:ChecklistItem) {
@@ -116,34 +115,4 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     func dataFilePath() -> URL {
         return documentsDirectory().appendingPathComponent("Checklists.plist")
     }
-    
-    func saveChecklistItems() {
-      // 1
-      let encoder = PropertyListEncoder()
-      // 2
-      do {
-        // 3
-        let data = try encoder.encode(items)
-        // 4
-        try data.write(
-          to: dataFilePath(),
-          options: Data.WritingOptions.atomic)
-        // 5
-      } catch {
-        // 6
-        print("Error encoding item array: \(error.localizedDescription)")
-      }
     }
-    
-    func loadChecklistItems() {
-        let path = dataFilePath()
-        if let data = try? Data(contentsOf: path) {
-            let decoder = PropertyListDecoder()
-            do {
-                items = try decoder.decode([ChecklistItem].self, from: data)
-            } catch {
-                print("Error decoding item array: \(error.localizedDescription)")
-            }
-        }
-    }
-}
